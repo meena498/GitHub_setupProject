1bfc0143bf5223719959c3266f17c331dcd12b5f
Meena498 <HP@DESKTOP-4RJMPTM>
2025-03-10 18:23:58 +0530

    add message

--- GitHub_setupProject/Controllers/HomeController.cs
+++ GitHub_setupProject/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace GitHub_setupProject.Controllers
         public ActionResult Index()
         {
             string var = "shubham";
+            string vr = "shubham";
             Console.WriteLine("Hello");
 
             return View();
